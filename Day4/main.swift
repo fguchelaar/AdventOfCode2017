a0895b7d867731a345c8710fad2c9cf05d095e4c
Frank Guchelaar <fguchelaar@gmail.com>
2017-12-04 20:27:10 +0100

    By sorting the letters of all the words, I can reuse the check for part 1. `combinations` is no longer needed

--- Day4/main.swift
+++ Day4/main.swift
@@ -1,22 +1,5 @@
 import Foundation
 
-extension Collection {
-    func combinations(n: Int) -> [[Element]] {
-        
-        if n == 0 {
-            return [[]]
-        }
-        if self.isEmpty {
-            return []
-        }
-        
-        let x = self.first!
-        let xs = Array(self.dropFirst())
-        
-        return xs.combinations(n: n-1).map { [x] + $0 } + xs.combinations(n: n)
-    }
-}
-
 let input = try! String(contentsOfFile: "day4-input.txt")
 let rows = input.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: .newlines)
 
@@ -31,9 +14,8 @@ func validPassphrases(phrases: [String]) -> [String] {
 }
 
 func containsAnagram(passphrase: String) -> Bool {
-    
-    let combinations = passphrase.components(separatedBy: " ").combinations(n: 2)
-    return combinations.contains { $0[0].sorted() == $0[1].sorted() }
+    let sortedByCharacters = passphrase.components(separatedBy: " ").map { String($0.sorted()) }
+    return !check(passphrase: sortedByCharacters.joined(separator: " "))
 }
 
 let validPartOne = validPassphrases(phrases: rows).count
