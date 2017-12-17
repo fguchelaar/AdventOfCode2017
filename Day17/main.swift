ba97e65add73ee0142493edea37871e77bf68724
Frank Guchelaar <fguchelaar@gmail.com>
2017-12-17 11:18:44 +0100

    Altered the loop, to hold the latest value at position `0` instead of maintaining an array with all insert-positions.

--- Day17/main.swift
+++ Day17/main.swift
@@ -5,7 +5,7 @@ let input = 386
 var buffer = [0]
 var currentPosition = 0
 for i in 1...2017 {
-    currentPosition = ((currentPosition + input) % buffer.count) + 1
+    currentPosition = ((currentPosition + input) % i) + 1
     buffer.insert(i, at: currentPosition)
 }
 
@@ -14,14 +14,13 @@ let pos2017: Int = buffer.index(of: 2017)!
 print("Part one: \(buffer[(pos2017 + 1) % buffer.count])")
 
 // MARK: Part two, calculate all insert-postions, find highest with `1`
-var insertPositions = [0]
+var valueAfter0 = -1
+var insertPosition = 0
 for i in 1...50000000 {
-    insertPositions.append(((insertPositions[i-1] + input) % i) + 1)
-}
-
-for i in stride(from: insertPositions.count-1, to: 0, by: -1) {
-    if insertPositions[i] == 1 {
-        print("Part two: \(i)")
-        break
+    insertPosition = (insertPosition + input) % i
+    if insertPosition == 0 {
+        valueAfter0 = i
     }
+    insertPosition += 1
 }
+print("Part two: \(valueAfter0)")
